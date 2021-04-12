476c9e61777436ae95834ed57910a9c3c8f79d39
Desi <desi.devaul@gmail.com>
2021-04-11 22:43:42 -0400

    server change

--- app.js
+++ app.js
@@ -49,8 +49,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const urls = [ /\localhost/,]; 
-app.use( cors({ origin: urls, credentials:true, }));
+const urls = [ /\localhost:5000/,]; 
+app.use( cors({ origin: urls, credentials: true, }));
   
 /* 
 --------------------------------------------------------------------------------
