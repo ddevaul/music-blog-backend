0e65301da7396320cc5185158d9e1ffccaaff100
Desi <desi.devaul@gmail.com>
2021-04-11 22:42:28 -0400

    server change

--- app.js
+++ app.js
@@ -48,7 +48,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+
+const urls = [ /\localhost/,]; 
+app.use( cors({ origin: urls, credentials:true, }));
+  
 /* 
 --------------------------------------------------------------------------------
 Routes
--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/loginController');
-
+const cors = require('cors');
 router.get('/', controller.index);
 
-router.post('/', controller.login);
+
+router.post('/', cors(), controller.login);
 
 module.exports = router; 
 
